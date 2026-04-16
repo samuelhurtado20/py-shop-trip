962b4399f5916725e59fda880e607a1379d8a54b
Samuel Hurtado <samuelhurtado20@hotmail.com>
2026-04-16 18:58:08 -0500

    Refactor Customer class methods and type hints

--- app/customer.py
+++ app/customer.py
@@ -2,7 +2,7 @@ import math
 
 
 class Customer:
-    def __init__(self, data: dict):
+    def __init__(self, data: dict) -> None:
         self.name = data["name"]
         self.product_cart = data["product_cart"]
         self.location = data["location"]
@@ -11,16 +11,15 @@ class Customer:
 
     def get_distance(self, target_location: list[int]) -> float:
         return math.sqrt(
-            (self.location[0] - target_location[0]) ** 2 +
-            (self.location[1] - target_location[1]) ** 2
+            (self.location[0] - target_location[0]) ** 2
+            + (self.location[1] - target_location[1]) ** 2
         )
 
-    def calculate_trip_cost(self, shop, fuel_price: float) -> float:
+    def calculate_trip_cost(self, shop: any, fuel_price: float) -> float:
         distance = self.get_distance(shop.location)
-        # Ida y vuelta
         fuel_needed = (distance * 2 * self.car["fuel_consumption"]) / 100
         fuel_cost = fuel_needed * fuel_price
-
+        
         product_cost = sum(
             self.product_cart[item] * shop.products[item]
             for item in self.product_cart

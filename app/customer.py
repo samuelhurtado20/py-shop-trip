caef56b4ebf6559f2827a2a37b7fe68d17edb28d
Samuel Hurtado <samuelhurtado20@hotmail.com>
2026-04-16 18:51:23 -0500

    Create customer.py

---
+++ app/customer.py
@@ -0,0 +1,28 @@
+import math
+
+
+class Customer:
+    def __init__(self, data: dict):
+        self.name = data["name"]
+        self.product_cart = data["product_cart"]
+        self.location = data["location"]
+        self.money = data["money"]
+        self.car = data["car"]
+
+    def get_distance(self, target_location: list[int]) -> float:
+        return math.sqrt(
+            (self.location[0] - target_location[0]) ** 2 +
+            (self.location[1] - target_location[1]) ** 2
+        )
+
+    def calculate_trip_cost(self, shop, fuel_price: float) -> float:
+        distance = self.get_distance(shop.location)
+        # Ida y vuelta
+        fuel_needed = (distance * 2 * self.car["fuel_consumption"]) / 100
+        fuel_cost = fuel_needed * fuel_price
+
+        product_cost = sum(
+            self.product_cart[item] * shop.products[item]
+            for item in self.product_cart
+        )
+        return fuel_cost + product_cost
